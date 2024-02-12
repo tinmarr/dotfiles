1a4dcf9f55787800b104c3e135e868cbcdb7a053
Martin Chaperot <martin.chapino@gmail.com>
2024-02-12 18:29:18 -0500

    Emacs!

---
+++ .config/emacs/init.el
@@ -0,0 +1,32 @@
+;; Clean up UI
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+(menu-bar-mode -1)
+
+;; Font
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font Mono" :height 128)
+
+;; Theme
+(load-theme 'tango-dark)
+
+;; Setup Package Manager
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
