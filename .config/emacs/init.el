5c05a6addc70cadaf5c3149d16f723ccab6e3d84
Martin Chaperot <martin.chapino@gmail.com>
2024-02-13 18:08:21 -0500

    Solid emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,22 +1,8 @@
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 10)
-
-(menu-bar-mode -1)
-
-;; Font
-(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font Mono" :height 110)
-
-
 ;; Setup Package Manager
 (require 'package)
 
-(add-to-list 'package-archives '(("melpa" . "https://melpa.org/packages/")
-				("org" . "https://orgmode.org/elpa/")
-				("elpa" . "https://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -28,43 +14,85 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Theme
-(use-package dracula-theme
-  :config
-  (load-theme 'dracula t))
-
-;; evil
 (use-package evil
   :init
   (setq evil-undo-system 'undo-redo)
   (setq evil-want-C-d-scroll t)
   (setq evil-want-C-u-scroll t)
   (setq evil-search-module 'swiper)
+  (setq evil-auto-indent nil)
   :config
   (evil-mode 1))
 
-;; ivy (minibuffer auto complete)
-(use-package ivy
-  :bind (:map ivy-minibuffer-map
-	 ("C-l" . ivy-alt-done)
-	 ("TAB" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line))
+(use-package general
+  :after evil
   :config
-  (ivy-mode 1))
+  (general-create-definer spc-def
+    :states 'normal
+    :prefix "SPC")
+  (spc-def org-mode-map "i" 'org-edit-latex-fragment)
+  (general-define-key :states '(normal insert) "C-p" 'find-file)
+)
 
-(use-package swiper)
+(use-package dracula-theme
+  :config
+  (load-theme 'dracula t))
+
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font Mono" :height 110)
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+
+(menu-bar-mode -1)
 
 ;; Line numbers
 (column-number-mode)
 (setq-default display-line-numbers 'relative)
 
-;; Rainbow Delims
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-;; Auto Tangleing
+(use-package org
+  :config
+  (setq org-hide-emphasis-markers t)
+  (setq org-startup-indented t))
+  (setq org-startup-with-latex-preview t)
+
+(use-package org-superstar
+  :hook (org-mode . (lambda () (org-superstar-mode 1)))
+  :config
+  (setq org-superstar-leading-bullet "  ")
+  (setq org-superstar-special-todo-items t))
+
+(setq org-format-latex-options 
+  '(:foreground default 
+    :background default 
+    :scale 3
+    :html-foreground "Black" 
+    :html-background "Transparent" 
+    :html-scale 1.0 
+    :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
+(add-hook 'org-mode-hook
+  (lambda ()
+      (add-hook 'after-save-hook (lambda () (org-latex-preview)))))
+
+(use-package ivy
+:bind (:map ivy-minibuffer-map
+	("C-l" . ivy-alt-done)
+	("TAB" . ivy-alt-done)
+	("C-j" . ivy-next-line)
+	("C-k" . ivy-previous-line))
+:config
+(ivy-mode 1))
+
+(use-package swiper)
+
 (add-hook 'org-mode-hook
     (lambda ()
 	(add-hook 'after-save-hook #'org-babel-tangle
 		nil 'make-it-local)))
+
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
