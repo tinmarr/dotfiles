57452c250ca0938785ebba573e0bce5c0f76a24c
Martin Chaperot <martin.chapino@gmail.com>
2024-04-04 22:58:57 -0400

    Small tweaks

--- .config/hypr/bg-rotate.sh
+++ .config/hypr/bg-rotate.sh
@@ -7,14 +7,12 @@ imgs=$(ls -1 $bg_path)
 
 monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
-for i in $imgs; do
-    hyprctl hyprpaper preload "$bg_path""$i"
-done
-
-while true; do
+#while true; do
     bg=$(echo "$imgs" | shuf -n 1)
+    hyprctl hyprpaper preload "$bg_path""$bg"
     for j in $monitors; do
 	hyprctl hyprpaper wallpaper "$j"",""$bg_path""$bg"
     done
-    sleep 10m
-done
+    hyprctl hyprpaper unload unused
+#    sleep 10m
+#done
