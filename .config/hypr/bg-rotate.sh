20363847a9e8290ec5c162efcde165481cbbfcd7
Martin Chaperot <martin.chapino@gmail.com>
2024-04-07 17:30:04 -0400

    Add hyprcursor for my cursor theme

--- .config/hypr/bg-rotate.sh
+++ .config/hypr/bg-rotate.sh
@@ -7,12 +7,12 @@ imgs=$(ls -1 $bg_path)
 
 monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
-#while true; do
+while true; do
     bg=$(echo "$imgs" | shuf -n 1)
     hyprctl hyprpaper preload "$bg_path""$bg"
     for j in $monitors; do
 	hyprctl hyprpaper wallpaper "$j"",""$bg_path""$bg"
     done
     hyprctl hyprpaper unload unused
-#    sleep 10m
-#done
+    sleep 10m
+done
