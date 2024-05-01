0d85b1ed342d9cb116239c2c98fdde7f3d9675c6
Martin Chaperot <martin.chapino@gmail.com>
2024-05-01 09:42:30 -0400

    Fix insert hotkey

--- dot-config/emacs/init.el
+++ dot-config/emacs/init.el
@@ -268,7 +268,7 @@
     '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
     '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
   )
-  (evil-define-key 'normal 'org-mode-map "i" 'org-edit-latex-fragment)
+  (evil-define-key 'normal 'org-mode-map (kbd "<leader> i") 'org-edit-latex-fragment)
 )
 
 (use-package org-superstar
