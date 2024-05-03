1051983dfbc85f3009de1b6d031d03fd3e24ec95
Martin Chaperot <martin.chapino@gmail.com>
2024-05-03 19:12:29 -0400

    new plan

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#dec5e2", "#060410" },
-	[SchemeSel] = { "#dec5e2", "#553296" },
-	[SchemeOut] = { "#dec5e2", "#7A84EA" },
-};
--- wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#060410";
-static const char* selfgcolor   = "#dec5e2";
-static const char* normbgcolor  = "#524BB4";
-static const char* normfgcolor  = "#dec5e2";
-static const char* urgbgcolor   = "#553296";
-static const char* urgfgcolor   = "#dec5e2";
