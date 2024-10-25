92c5eec32affb0775bbf92c009f1dfdcaaa3a0f3
Martin Chaperot <martin.chapino@gmail.com>
2024-10-25 12:54:35 -0400

    zshrc: quick cd then vim script

--- dot-config/nvim/lua/user/options.lua
+++ dot-config/nvim/lua/user/options.lua
@@ -1,5 +1,6 @@
 -- Vim options documented here: https://neovim.io/doc/user/quickref.html#option-list
 vim.opt.tabstop = 4
+vim.opt.expandtab = true
 vim.opt.shiftwidth = 0
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
