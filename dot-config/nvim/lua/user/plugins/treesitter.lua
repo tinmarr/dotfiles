f62826bb46a3bbf2a101e9d9b7fde2abec0d6abd
Martin Chaperot <martin.chapino@gmail.com>
2024-10-25 13:57:23 -0400

    hypr: remove readme.org and use traditional config instead

--- dot-config/nvim/lua/user/plugins/treesitter.lua
+++ dot-config/nvim/lua/user/plugins/treesitter.lua
@@ -1,21 +1,24 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	ft = { "lua", "go" },
-	config = function()
-		require("nvim-treesitter.configs").setup {
-			ensure_installed = { "lua" },
-			auto_install = true,
-			sync_install = false,
-			ignore_install = { "org" },
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false
-			},
-			indent = {
-				enable = true
-			},
-			modules = {} -- doesn't do anything... for lsp
-		}
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = { "lua", "hyprlang" },
+            auto_install = true,
+            sync_install = false,
+            ignore_install = { "org" },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false
+            },
+            indent = {
+                enable = true
+            },
+            modules = {} -- doesn't do anything... for lsp
+        }
+
+        vim.filetype.add({
+            pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+        })
+    end,
 }
