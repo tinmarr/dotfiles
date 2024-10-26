e93d027a49e278e5bb7d03ac008f659248c151fe
Martin Chaperot <martin.chapino@gmail.com>
2024-10-25 21:06:41 -0400

    nvim: update org mode for latx

--- dot-config/nvim/lua/user/plugins/orgmode.lua
+++ dot-config/nvim/lua/user/plugins/orgmode.lua
@@ -1,9 +1,10 @@
 return {
-	"nvim-orgmode/orgmode",
-	event = "VeryLazy",
-	ft = { "org" },
-	opts = {
-		org_agenda_files = "~/notes/**/*",
-		org_default_notes_file = "~/notes/refile.org",
-	}
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    opts = {
+        org_agenda_files = "~/notes/**/*",
+        org_default_notes_file = "~/notes/refile.org",
+        org_highlight_latex_and_related = "entities",
+    }
 }
--- dot-config/nvim/lua/user/plugins/telescope.lua
+++ dot-config/nvim/lua/user/plugins/telescope.lua
@@ -20,8 +20,7 @@ return {
         },
         keys = {
             { "<C-p>",      "<cmd>Telescope git_files show_untracked=true<cr>",                               desc = "Find git file" },
-            { "<leader>ff", "<cmd>Telescope git_files show_untracked=true<cr>",                               desc = "Find git file" },
-            { "<leader>fF", "<cmd>Telescope find_files<cr>",                                                  desc = "Find file" },
+            { "<leader>ff", "<cmd>Telescope find_files<cr>",                                                  desc = "Find file" },
             { "<leader>fh", "<cmd>Telescope find_files hidden=true no_ignore=true no_ignore_parent=true<cr>", desc = "Find hidden file" },
             { "<leader>fg", "<cmd>Telescope live_grep<cr>",                                                   desc = "Grep through working dir" },
             { "<leader>f/", "<cmd>Telescope current_buffer_fuzzy_find<cr>",                                   desc = "Fuzzy find buffer" },
