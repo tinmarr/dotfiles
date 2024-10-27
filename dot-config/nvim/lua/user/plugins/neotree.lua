ce230e3c01137722bc0f4de8a9ed333e86a3d455
Martin Chaperot <martin.chapino@gmail.com>
2024-10-26 21:33:53 -0400

    nvim: update neotree

--- dot-config/nvim/lua/user/plugins/neotree.lua
+++ dot-config/nvim/lua/user/plugins/neotree.lua
@@ -1,26 +1,26 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	keys = {
-		{ "<leader>e", "<cmd>Neotree<cr>", desc = "Open Neotree" },
-	},
-	opts = {
-		close_if_last_window = true,
-		window = {
-			position = "right",
-		},
-		filesystem = {
-			filtered_items = {
-				visible = true
-			},
-			follow_current_file = {
-				enabled = true
-			},
-		}
-	},
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    keys = {
+        { "<leader>e", "<cmd>Neotree<cr>", desc = "Open Neotree" },
+    },
+    opts = {
+        close_if_last_window = true,
+        window = {
+            position = "right",
+        },
+        filesystem = {
+            filtered_items = {
+                visible = true
+            },
+            follow_current_file = {
+                enabled = true
+            },
+        },
+    },
 }
