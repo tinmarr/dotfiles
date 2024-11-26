ce8b7ab1e3270259dfd838981dbab5db1afe582f
Martin Chaperot <martin.chapino@gmail.com>
2024-11-26 18:15:03 -0500

    nvim: remove latex conceal

--- dot-config/nvim/lua/user/plugins/tex.lua
+++ dot-config/nvim/lua/user/plugins/tex.lua
@@ -8,7 +8,6 @@ return {
             vim.g.vimtex_view_method = 'zathura'
             vim.g.vimtex_quickfix_mode = 0
             vim.g.tex_conceal = 'abdmg'
-            vim.opt.conceallevel = 1
         end
     }
 }
