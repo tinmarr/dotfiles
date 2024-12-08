bef5d2a80edb332ac1b82b094a577596185dc51f
Martin Chaperot <martin.chapino@gmail.com>
2024-12-08 14:32:11 -0500

    nvim: trim trailing whitespace

--- dot-config/nvim/lua/user/options.lua
+++ dot-config/nvim/lua/user/options.lua
@@ -6,3 +6,19 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.termguicolors = true
+
+vim.api.nvim_create_augroup('bufcheck', { clear = true })
+
+-- highlight yanks
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group    = 'bufcheck',
+    pattern  = '*',
+    callback = function() vim.highlight.on_yank { timeout = 500 } end
+})
+
+-- remove trailing whitespace
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "bufcheck",
+    pattern = "*",
+    callback = function() vim.cmd("%s/\\s\\+$//e") end
+})
