c90704ce165ab566c39f49b7ef8a2ab057629d6a
Martin Chaperot <martin.chapino@gmail.com>
2024-12-11 12:17:04 -0500

    nvim: add nice pane controls

--- dot-config/nvim/lua/user/keymaps.lua
+++ dot-config/nvim/lua/user/keymaps.lua
@@ -9,3 +9,12 @@ vim.keymap.set("n", "<leader>bk", "<cmd>b#<cr>", { desc = "Goto last accessed bu
 vim.keymap.set("n", "<leader>bh", vim.cmd.bprevious, { desc = "Goto previous buffer" })
 vim.keymap.set("n", "<leader>bl", vim.cmd.bnext, { desc = "Goto previous buffer" })
 vim.keymap.set("n", "<leader>bc", vim.cmd.bdelete, { desc = "Close buffer" })
+
+vim.keymap.set("n", "<M-S-l>", "5<C-w>>")
+vim.keymap.set("n", "<M-S-h>", "5<C-w><")
+vim.keymap.set("n", "<M-S-k>", "2<C-w>+")
+vim.keymap.set("n", "<M-S-j>", "2<C-w>-")
+vim.keymap.set("n", "<M-v>", "<C-w>v")
+vim.keymap.set("n", "<M-s>", "<C-w>s")
+vim.keymap.set("n", "<M-c>", "<C-w>c")
+vim.keymap.set("n", "<M-=>", "<C-w>=")
