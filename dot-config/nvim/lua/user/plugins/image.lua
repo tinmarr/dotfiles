1f374a407fa4e3d70e73e75cad7c6b3438dcfa0a
Martin Chaperot <martin.chapino@gmail.com>
2025-01-03 16:08:16 -0500

    nvim: clean up some stuff

--- dot-config/nvim/lua/user/plugins/image.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "3rd/image.nvim",
-    lazy = true,
-    config = function()
-        -- ...
-    end,
-}
--- dot-config/nvim/lua/user/plugins/notify.lua
+++ dot-config/nvim/lua/user/plugins/notify.lua
@@ -1,6 +1,9 @@
 return {
     "rcarriga/nvim-notify",
     event = "VeryLazy",
+    opts = {
+        background_colour = "#000000"
+    },
     init = function()
         vim.notify = require("notify")
     end
