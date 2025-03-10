0315a8ab089184f3be6ff3627754de925b89d3b5
Martin Chaperot <martin.chapino@gmail.com>
2025-03-09 21:55:13 -0700

    nvim: improve telescope

--- dot-config/nvim/lua/user/plugins/telescope.lua
+++ dot-config/nvim/lua/user/plugins/telescope.lua
@@ -16,6 +16,12 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-fzf-native.nvim",
+            "BurntSushi/ripgrep",
+            "sharkdp/fd",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make"
+            }
         },
         opts = {
             defaults = {
@@ -23,6 +29,8 @@ return {
                     i = {
                         ["<C-j>"] = "move_selection_next",
                         ["<C-k>"] = "move_selection_previous",
+                        ["<C-u>"] = "results_scrolling_up",
+                        ["<C-d>"] = "results_scrolling_down",
                         ["<esc>"] = "close",
                     }
                 }
@@ -34,6 +42,7 @@ return {
             { "<leader>fh", "<cmd>Telescope find_files hidden=true no_ignore=true no_ignore_parent=true<cr>", desc = "Find hidden file" },
             { "<leader>fg", "<cmd>Telescope live_grep<cr>",                                                   desc = "Grep through working dir" },
             { "<leader>f/", "<cmd>Telescope current_buffer_fuzzy_find<cr>",                                   desc = "Fuzzy find buffer" },
+            { "<leader>fs", "<cmd>Telescope treesitter<cr>",                                                  desc = "Find symbol" },
             { "<leader>bi", "<cmd>Telescope buffers<cr>",                                                     desc = "List buffers" },
         },
         config = function(_, opts)
@@ -41,5 +50,4 @@ return {
             require("telescope").load_extension("fzf")
         end,
     },
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true }
 }
