d5342a5a8ba7a15cfd4439c95a781b1deb18d494
Martin Chaperot <martin.chapino@gmail.com>
2025-03-26 10:12:30 -0400

    hypr: fix monitor toggling

--- dot-config/hypr/switch.sh
+++ dot-config/hypr/switch.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-if grep open /proc/acpi/button/lid/LID/state; then
+if grep open /proc/acpi/button/lid/LID0/state; then
     hyprctl keyword monitor "eDP-1, preferred, auto, 1"
+    systemctl --user restart waybar.service
 else
     if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
         hyprctl keyword monitor "eDP-1, disable"
+        systemctl --user restart waybar.service
     fi
 fi
