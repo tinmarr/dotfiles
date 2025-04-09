e30b1b194631dd964683513f4ce96a1632c01fb6
Martin Chaperot <martin.chapino@gmail.com>
2025-04-09 15:27:25 -0400

    hypr: fix some things

--- dot-config/hypr/sunsetctl.sh
+++ dot-config/hypr/sunsetctl.sh
@@ -3,17 +3,20 @@
 max_temp=6500
 min_temp=2500
 
-start_time=1800
-full_time=2000
-end_time=7
+scan_time=1730 # time we start checking for gradient
+start_time=1800 # time we start gradient
+full_time=2000 # time we end gradient
+end_time=0700
 
 while true; do
     current_time=$(date +%H%M)
 
-    if [[ "$current_time" -lt "$start_time" && "$current_time" -gt "$end_time" ]]; then
+    if [[ "$current_time" -lt "$scan_time" && "$current_time" -gt "$end_time" ]]; then
         echo "reset"
         hyprctl -i 0 hyprsunset identity
         sleep 1h
+    elif [[ "$current_time" -lt "$start_time" ]]; then
+        sleep 10m
     elif [[ "$current_time" -ge "$start_time" && "$current_time" -le "$full_time" ]]; then
         lerp_t=$(python -c "print(($current_time - $start_time) / ($full_time - $start_time))" )
         temp_value=$(python -c "print($max_temp + ($min_temp - $max_temp) * $lerp_t)" )
