a534a4553bd88f8dd766b0b688fc24e2ae6ffd4a
Martin Chaperot <martin.chapino@gmail.com>
2025-04-16 07:55:06 -0400

    hypr: fix lid switcher

--- dot-config/hypr/switch.sh
+++ dot-config/hypr/switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-    hyprctl keyword monitor "eDP-1, preferred, auto, 1"
+    hyprctl keyword monitor "eDP-1, preferred, auto, 1.33"
     systemctl --user restart waybar.service
 else
     if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
