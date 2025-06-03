8b1322e9c4d53f57857f016c1a3625a0fbe189a4
Martin Chaperot <martin.chapino@gmail.com>
2025-06-03 09:39:51 -0400

    nvim: better gitsigns config

--- dot-config/nvim/lua/user/plugins/gitsigns.lua
+++ dot-config/nvim/lua/user/plugins/gitsigns.lua
@@ -1,5 +1,20 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {}
+    keys = {
+        { "<leader>gb", "<cmd>Gitsigns blame<cr>",               desc = "Show full file blame" },
+        { "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>",          desc = "Reset hunk" },
+        { "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<cr>", desc = "Preview hunk" },
+    },
+    opts = {
+        current_line_blame = true,
+        current_line_blame_opts = {
+            virt_text = true,
+            virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+            delay = 1000,
+            ignore_whitespace = false,
+            virt_text_priority = 100,
+            use_focus = true,
+        },
+    }
 }
