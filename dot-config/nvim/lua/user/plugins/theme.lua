9f8e612b634d52a8f5cc53b907d4e5a820791ac2
Martin Chaperot <martin.chapino@gmail.com>
2025-06-03 09:53:53 -0400

    nvim: fix theme?

--- dot-config/nvim/lua/user/plugins/theme.lua
+++ dot-config/nvim/lua/user/plugins/theme.lua
@@ -1,20 +1,18 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        lazy = false,
-        priority = 1000,
-        ---@module "catppuccin"
-        ---@type CatppuccinOptions
-        opts = {
-            flavour = "mocha",
-            integrations = {
-                which_key = true
-            }
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd.colorscheme "catppuccin"
-        end
-    }
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    ---@module "catppuccin"
+    ---@type CatppuccinOptions
+    opts = {
+        flavour = "mocha",
+        integrations = {
+            which_key = true
+        }
+    },
+    config = function(_, opts)
+        require("catppuccin").setup(opts)
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
