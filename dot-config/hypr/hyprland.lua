8f0cf277784a1be7af17748cc50c5366176c751b
Martin Chaperot <martin.chaperot@proton.me>
2026-09-23 16:48:51 -0400

    quickshell: proper reload

--- dot-config/hypr/hyprland.lua
+++ dot-config/hypr/hyprland.lua
@@ -67,7 +67,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("app2unit -- awww-randomize")
     hl.exec_cmd("app2unit -- walker --gapplication-service")
     hl.exec_cmd("~/.config/hypr/brightness-menu.sh update-cache")
-    hl.exec_cmd("app2unit -- qs")
+    hl.exec_cmd("app2unit -u qs.service -t service -s a -- qs")
 end)
 
 -----------------------
@@ -325,7 +325,7 @@ local function reload()
         "app2unit -u sunsetctl.service -t service -s b -- systemd-cat -t sunsetctl ~/.config/hypr/sunsetctl.sh")
     hl.exec_cmd("systemctl stop --user awww-daemon.service")
     hl.exec_cmd("setsid -f app2unit -u awww-daemon.service -t service -s b -- awww-daemon")
-    hl.exec_cmd("pkill qs; app2unit -- qs")
+    hl.exec_cmd("systemctl --user stop qs; app2unit -u qs.service -t service -s a -- qs")
 end
 
 hl.bind(mainMod .. " + q", hl.dsp.window.close())
